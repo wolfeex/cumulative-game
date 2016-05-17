bbb2f4f35805c3c31c16872954603a4a18f2df05
Tanguy DECHIRON <tanguy.dechiron@gmail.com>
2016-05-17 14:37:18 +0200

    adding win condition (wonder construction)

--- js/check.js
+++ js/check.js
@@ -2,12 +2,18 @@
 // Check for victory / lose conditions
 (function(game, $) {
   game.check = function() {
-    // Lost last population => lose
+    // Lost last population => LOSE
     if(game.population.count <= 0) {
       game.log.add(game.renderer.langFile['log-message-game-lose']);
       return true;
     }
-    
+
+    // Wonder has been contructed => WIN
+    if(game.buildings.wonder.count == 1) {
+      game.log.add(game.renderer.langFile['log-message-game-win']);
+      return true;
+    }
+
     return false;
   };
 })(window.game = window.game || {}, jQuery);
